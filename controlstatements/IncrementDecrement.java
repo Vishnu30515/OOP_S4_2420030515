7f08139e6a2e8f49ecb0fdaca7d5e0ac622e3f1a
Vishnu30515 <karusoduvishnuvardhan@gmail.com>
2025-08-02 11:34:05 +0530

    Update IncrementDecrement.java

--- controlstatements/IncrementDecrement.java
+++ controlstatements/IncrementDecrement.java
@@ -6,12 +6,13 @@ public class IncrementDecrement {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		for(int i=1;i<10;i++) {
+		for(int i=1;i<6;i++) {
 			System.out.println(i);
 		}
 		for(int i=10;i>=1;i--) {
 			System.out.println(i);
 		}
 	}
+
 
 }
